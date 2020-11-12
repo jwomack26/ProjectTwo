1990ce90924b1e530bef74a86cc9a9153b785c0c
Jerilyn Womack <jerilynwomack@Jerilyns-MBP-2.attlocal.net>
2020-11-12 13:05:18 -0600

    Updated Java

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,11 +1,15 @@
 function search()
 {
    var input = $('#input').val();
-   $.get('http://api.giphy.com/v1/gifs/search?q='+input+'&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF', function(response) {
+   $.get("http://api.giphy.com/v1/gifs/search?q='&limit=1'"+input+"&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF", function(response) {
        $('#img').html("<img src="+ response.data[0].images.downsized_large.url + ">");
 
+    for(let image of response.images){
+      resultHtml = resultHtml + '<img src=${image.images.downsized_large.url} />'
+      }
+  $('#img').thml(resultsHtml)
 }) 
-
-
 }
+
+
 
