5534a99a185c050c05dd50d91a9b632292533fcb
Jerilyn Womack <jerilynwomack@Jerilyns-MBP-2.attlocal.net>
2020-11-12 17:04:18 -0600

    Got javascript to work and show multiple images

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,15 +1,12 @@
-function search()
-{
-   var input = $('#input').val();
-   $.get("http://api.giphy.com/v1/gifs/search?q='&limit=1'"+input+"&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF", function(response) {
-       $('#img').html("<img src="+ response.data[0].images.downsized_large.url + ">");
-
-    for(let image of response.images){
-      resultHtml = resultHtml + '<img src=${image.images.downsized_large.url} />'
-      }
-  $('#img').thml(resultsHtml)
-}) 
+function search(){
+  var resultHtml; var input=$('#input').val(); console.log(input) 
+  $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF&quot;", function(response){
+    console.log(response.data[0]);
+    $('#img').html("<img src="+ response.data[0].images.downsized_large.url + ">");
+    
+    for(let image of response.data){
+      resultHtml = resultHtml + '<img src=' +image.images.downsized_large.url + ">"
+    }
+    $('#img').html(resultHtml)
+  })
 }
-
-
-
