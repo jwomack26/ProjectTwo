85af0030a2ae0c0f163e9ee3e285ae131f6899ac
Jerilyn Womack <jerilynwomack@Jerilyns-MBP-2.attlocal.net>
2020-11-13 19:54:42 -0600

    Changed API url from http to https

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function search(){
   var resultHtml; var input=$('#input').val(); console.log(input) 
-  $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF&quot;", function(response){
+  $.get("https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=MKnhlA6fn7IktUX3r5DzPOtEipLKtXbF&quot;", function(response){
     console.log(response.data[0]);
     $('#img').html("<img src="+ response.data[0].images.downsized_large.url + ">");
     
